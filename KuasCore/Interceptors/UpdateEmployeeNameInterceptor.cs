6d6efa621663d238f5aa291807371e19ac317b78
1101137215 <black25520@yahoo.com.tw>
2015-04-17 16:27:54 +0800

    homework

---
+++ KuasCore/Interceptors/UpdateEmployeeNameInterceptor.cs
@@ -0,0 +1,37 @@
+﻿using AopAlliance.Intercept;
+using KuasCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuasCore.Interceptors
+{
+    class UpdateEmployeeNameInterceptor : IMethodInterceptor
+    {
+        public object Invoke(IMethodInvocation invocation)
+        {
+            Console.WriteLine("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            Debug.Print("DebogLogInterceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+
+            object result = invocation.Proceed();
+
+            if (result is Employee)
+            {
+                Employee emp = (Employee)result;
+                emp.Name = emp.Name + "hello";
+                result = emp;
+            }
+
+
+            Console.WriteLine("回傳的資料已取得 [{0}]", result);
+            Debug.Print("回傳的資料已取得 [{0}]", result);
+
+            return result;
+        }
+
+
+    }
+}
